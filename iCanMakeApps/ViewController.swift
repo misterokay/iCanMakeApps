cf684aaa2e63947addd67c990122b04bfe7eeed1
Kevin Ok <kevinok@Kevins-MBP.att.net>
2016-04-07 17:31:58 -0700

    added data to array

--- iCanMakeApps/ViewController.swift
+++ iCanMakeApps/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        data = ["Amy", "Bob", "Chris"]
         // Do any additional setup after loading the view, typically from a nib.
     }
 
